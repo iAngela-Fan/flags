5b6aa8f49cc427aee1ea59e37fec1b2a5d35e741
iAngela-Fan <132737040+iAngela-Fan@users.noreply.github.com>
2023-05-08 17:44:17 +0800

    Add files via upload

---
+++ flag_Hungary.py
@@ -0,0 +1,22 @@
+def tri_flag(c1,c2,c3):
+    a=[c1,c2,c3]
+    for i in range(3):
+       t.up()
+       t.goto(200,100-200*i/3)
+       t.down()
+       t.color(a[i])
+       t.begin_fill()
+       for j in range(2):
+            t.right(90)
+            t.forward(200/3)
+            t.right(90)
+            t.forward(400)
+       t.end_fill()
+    
+import turtle as t
+t.clear()
+t.reset()
+t.Pen()
+t.pensize(4)
+tri_flag('red','white','green')
+t.done()
